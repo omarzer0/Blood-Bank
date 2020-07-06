c043b11897d9d846f4c55cfe24a5061ad5320599
Omar Adel <omarzero1015@gmail.com>
2020-07-06 18:43:00 +0200

    Add Splash screen

--- app/src/main/java/com/azapps/bloodbankipda3/view/activity/MainActivity.java
+++ app/src/main/java/com/azapps/bloodbankipda3/view/activity/MainActivity.java
@@ -1,6 +1,5 @@
 package com.azapps.bloodbankipda3.view.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.azapps.bloodbankipda3.R;
---
+++ app/src/main/java/com/azapps/bloodbankipda3/view/activity/splash/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.azapps.bloodbankipda3.view.activity.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.azapps.bloodbankipda3.R;
+import com.azapps.bloodbankipda3.view.activity.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    public static final int SPLASH_DELAYED_LENGTH = 1000 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },SPLASH_DELAYED_LENGTH );
+    }
+}
