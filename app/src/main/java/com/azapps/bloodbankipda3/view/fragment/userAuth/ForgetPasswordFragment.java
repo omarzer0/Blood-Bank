5828f06a7e9bcf08e2d1e2950b838a62cd0c0c38
Omar Adel <omarzero1015@gmail.com>
2020-07-10 23:50:01 +0200

    send verification code layout

--- app/src/main/java/com/azapps/bloodbankipda3/view/fragment/userAuth/ForgetPasswordFragment.java
+++ app/src/main/java/com/azapps/bloodbankipda3/view/fragment/userAuth/ForgetPasswordFragment.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.azapps.bloodbankipda3.R;
+import com.azapps.bloodbankipda3.helper.Utils;
 
-public class ForgetPasswordFragment extends Fragment {
+public class ForgetPasswordFragment extends Fragment implements View.OnClickListener {
+    Button senVerificationCodeBtn;
+
     public ForgetPasswordFragment(){
 
     }
@@ -23,11 +27,22 @@ public class ForgetPasswordFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forget_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_forget_password, container, false);
+        senVerificationCodeBtn = view.findViewById(R.id.fragment_forget_password_btn_send_verification_code_button);
+        senVerificationCodeBtn.setOnClickListener(this);
+        return view;
     }
 
     public static ForgetPasswordFragment newInstance() {
         return new ForgetPasswordFragment();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fragment_forget_password_btn_send_verification_code_button:
+                Utils.replaceFragments(ConfirmForgetPasswordFragment.newInstance(),getFragmentManager());
+                break;
+        }
+    }
 }
